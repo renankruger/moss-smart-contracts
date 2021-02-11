1de1f39bd561bce9be47da289c531afcfce2bb98
Renan Kruger <kruger.renan.eng@gmail.com>
2021-02-10 21:49:21 -0300

    Just Boost

--- token/.solcover.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  norpc: true,
-  deepSkip: true,
-  copyPackages: [
-    // 'zos-lib', // Including this package breaks coverage process due crappy code style in the zos-lib/.../App.sol
-    '@openzeppelin'
-  ]
-}
